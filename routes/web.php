5f2e6f496e68e4dbdc2deb0f21bd68f6fa7afab0
yudzero <firgiawantama88@gmail.com>
2022-09-07 09:32:40 +0700

    add-dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/hubin/perusahaan', function () {
+    return view('perusahaanhubin');
+});
